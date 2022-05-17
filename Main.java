52720646cbf8ec45d3010c8fc827808715372193
serafin625 <serafin.m625@gmail.com>
2022-05-17 09:29:11 +0000

    3_1a

--- Main.java
+++ Main.java
@@ -1,16 +1,35 @@
 import java.io.IOException;
-
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     try {
       Service1 s = new Service1();
       s.addStudent(new Student("Krzysztof", 20));
       s.addStudent(new Student("Janusz", 40));
-
+    
       var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
-      }
+        }
+      //3
+      Scanner wczytaj=new Scanner(System.in);
+      int opcja=0;
+      do{
+        System.out.println("***********MENU***********");
+        System.out.println("1. Dodawanie studenta");
+        opcja=wczytaj.nextInt();
+        switch (opcja){
+          case 1:{
+            System.out.println("imie: ");
+            String imie=wczytaj.next();
+            System.out.println("wiek: ");
+            int wiek=wczytaj.nextInt();
+            s.addStudent(new Student(imie, wiek));
+              
+            break;
+          }
+        }
+      }while(opcja!=0);
     } catch (IOException e) {
 
     }
